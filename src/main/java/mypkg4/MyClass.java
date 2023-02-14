fdbaf53faa1e3315ad6df6b967859285f6d0720f
deepanshi2704 <109263370+deepanshi2704@users.noreply.github.com>
2023-02-14 21:42:37 +0530

    first commit

---
+++ src/main/java/mypkg4/MyClass.java
@@ -0,0 +1,13 @@
+package mypkg4;
+
+public class MyClass {
+	public void show() {
+		System.out.println("Welcome to Jenkins");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		ob.show();
+	}
+
+}
