90ce0979c154a65d2eccb67b48a896efa8e9c2ce
deepanshi2704 <109263370+deepanshi2704@users.noreply.github.com>
2023-02-15 21:41:40 +0530

    new

--- src/main/java/mypkg4/MyClass.java
+++ src/main/java/mypkg4/MyClass.java
@@ -4,7 +4,12 @@ public class MyClass {
 	public void show() {
 		System.out.println("Welcome to Jenkins");
 	}
-
+	public void display() {
+		System.out.println("Hey");
+	}
+	public void f3() {
+		System.out.println("F3");
+	}
 	public static void main(String[] args) {
 		MyClass ob = new MyClass();
 		ob.show();
